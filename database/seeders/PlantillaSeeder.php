4661bc65e3fc5a4f0e8b7b888e4da5456f5997a8
julian garnica <juliang1012@gmail.com>
2024-02-09 14:40:01 -0500

    ajustando seeder plantilla

--- database/seeders/PlantillaSeeder.php
+++ database/seeders/PlantillaSeeder.php
@@ -24,70 +24,70 @@ class PlantillaSeeder extends Seeder
             ],
             [
                 'description' => 'plantilla aniversario 1',
-                'subject' =>',¡Feliz aniversario! Te desea la familia Lili Pink & Yoi',
+                'subject' =>',¡Felicitaciones por tu aniversario en la familia Lili Pink & Yoi!',
                 'type' => 'aniversario',
                 'url' => '/img/aniversario/1.jpg',
                 'ide' => '1468'
             ],
             [
                 'description' => 'plantilla aniversario 2',
-                'subject' =>',¡Feliz aniversario! Te desea la familia Lili Pink & Yoi',
+               'subject' =>',¡Felicitaciones por tu aniversario en la familia Lili Pink & Yoi!',
                 'type' => 'aniversario',
                 'url' => '/img/aniversario/2.jpg',
                 'ide' => '1469'
             ],
             [
                 'description' => 'plantilla aniversario 3',
-                'subject' =>',¡Feliz aniversario! Te desea la familia Lili Pink & Yoi',
+               'subject' =>',¡Felicitaciones por tu aniversario en la familia Lili Pink & Yoi!',
                 'type' => 'aniversario',
                 'url' => '/img/aniversario/3.jpg',
                 'ide' => '1470'
             ],
             [
                 'description' => 'plantilla aniversario 4',
-                'subject' =>',¡Feliz aniversario! Te desea la familia Lili Pink & Yoi',
+                'subject' =>',¡Felicitaciones por tu aniversario en la familia Lili Pink & Yoi!',
                 'type' => 'aniversario',
                 'url' => '/img/aniversario/4.jpg',
                 'ide' => '1471'
             ],
             [
                 'description' => 'plantilla aniversario 5',
-                'subject' =>',¡Feliz aniversario! Te desea la familia Lili Pink & Yoi',
+                'subject' =>',¡Felicitaciones por tu aniversario en la familia Lili Pink & Yoi!',
                 'type' => 'aniversario',
                 'url' => '/img/aniversario/5.jpg',
                 'ide' => '1472'
             ],
             [
                 'description' => 'plantilla aniversario 6',
-                'subject' =>',¡Feliz aniversario! Te desea la familia Lili Pink & Yoi',
+                'subject' =>',¡Felicitaciones por tu aniversario en la familia Lili Pink & Yoi!',
                 'type' => 'aniversario',
                 'url' => '/img/aniversario/6.jpg',
                 'ide' => '1473'
             ],
             [
                 'description' => 'plantilla aniversario 7',
-                'subject' =>',¡Feliz aniversario! Te desea la familia Lili Pink & Yoi',
+                'subject' =>',¡Felicitaciones por tu aniversario en la familia Lili Pink & Yoi!',
                 'type' => 'aniversario',
                 'url' => '/img/aniversario/7.jpg',
                 'ide' => '1474'
             ],
             [
                 'description' => 'plantilla aniversario 8',
-                'subject' =>',¡Feliz aniversario! Te desea la familia Lili Pink & Yoi',
+               'subject' =>',¡Felicitaciones por tu aniversario en la familia Lili Pink & Yoi!',
                 'type' => 'aniversario',
                 'url' => '/img/aniversario/8.jpg',
                 'ide' => '1475'
             ],
             [
                 'description' => 'plantilla aniversario 9',
-                'subject' =>',¡Feliz aniversario! Te desea la familia Lili Pink & Yoi',
+                'subject' =>',¡Felicitaciones por tu aniversario en la familia Lili Pink & Yoi!',
                 'type' => 'aniversario',
                 'url' => '/img/aniversario/9.jpg',
                 'ide' => '1476'
             ],
             [
                 'description' => 'plantilla aniversario 10',
-                'subject' =>',¡Feliz aniversario! Te desea la familia Lili Pink & Yoi',
+               'subject' =>',¡Felicitaciones por tu aniversario en la familia Lili Pink & Yoi!',
                 'type' => 'aniversario',
                 'url' => '/img/aniversario/10.jpg',
                 'ide' => '1477'
